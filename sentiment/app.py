cf3a1019936446bebf29a769c1d0701f4c0f392c
Ahmad Sohail Azizi <ahmadsohailazizi@Ahmads-MacBook-Pro-2.local>
2026-06-11 10:00:03 +0300

    Add GitHub Actions workflow

--- sentiment/app.py
+++ sentiment/app.py
@@ -14,4 +14,4 @@ def analyze(text):
     return jsonify({"text": text, "sentiment": sentiment})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
